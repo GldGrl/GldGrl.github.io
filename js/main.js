2ea016ae49081be03a518b0e745d9070d57ff564
Marco Kemmerling <contact@kemmerling.me>
2021-11-01 11:29:10 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -3,5 +3,9 @@ jQuery(document).ready(function( $ ) {
   $(".navbar-collapse a").on('click', function() {
     $(".navbar-collapse.collapse").removeClass('in');
   });
+
+  $(".btn").on('click', function() {
+    $(".buttonsuccess").html("Your message has been sent into the void. Thank you!");
+  });
   
 });
